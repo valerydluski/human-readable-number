371825031a956af7a7163089db014a761abfd86d
valerydluski <valerydluski@gmail.com>
2020-02-20 13:22:18 +0300

    feat: add result 10-19

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 module.exports = function toReadable (number) {
     let result = '';
-    let arr0_10 = ['zero','one','two','three','four','five','six','seven','eight','nine','ten'];
+    let arr0_9 = ['zero','one','two','three','four','five','six','seven','eight','nine'];
     if(number>=0 && number<=10){
-        result = arr0_10[number]
+        result = arr0_10[number];
         return result;
     }
-
+    let arr10_19 = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+    if(number>9 && number<20){
+        let ostatok=number%10;
+        result = arr10_19[ostatok];
+        return result;
+    }
 }
