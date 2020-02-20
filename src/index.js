2268eb62e85c304bf17e5504f1ee055a99498b4f
valerydluski <valerydluski@gmail.com>
2020-02-20 13:24:55 +0300

    fix: bag

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable (number) {
     let result = '';
     let arr0_9 = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-    if(number>=0 && number<=10){
-        result = arr0_10[number];
+    if(number>=0 && number<10){
+        result = arr0_9[number];
         return result;
     }
     let arr10_19 = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
