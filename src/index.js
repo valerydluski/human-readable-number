3b8da01b4a3b708f3c34e15a50b1e769494040f5
valerydluski <valerydluski@gmail.com>
2020-02-20 13:12:44 +0300

    fix: result for 0-10

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function toReadable (number) {
     let result = '';
-    let arr0_10 = ['zero','one','two','three','four','five','six','seven','ten'];
+    let arr0_10 = ['zero','one','two','three','four','five','six','seven','eight','nine','ten'];
     if(number>=0 && number<=10){
         result = arr0_10[number]
         return result;
     }
-    
+
 }
