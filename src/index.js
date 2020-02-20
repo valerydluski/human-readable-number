738468a002678c2ba67fc4cba00cc8f189fe5da3
valerydluski <valerydluski@gmail.com>
2020-02-20 13:09:35 +0300

    feat: add result for 0-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+    let result = '';
+    let arr0_10 = ['zero','one','two','three','four','five','six','seven','ten'];
+    if(number>=0 && number<=10){
+        result = arr0_10[number]
+        return result;
+    }
+    
 }
