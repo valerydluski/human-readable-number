37e2b4d828a68cbe4c711bd86afc7c0815eeb0d3
valerydluski <valerydluski@gmail.com>
2020-02-21 17:07:15 +0300

    feat: add one function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 module.exports = function toReadable (number) {
     let result = '';
     let arr0_9 = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    let arr10_19 = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    let arr3 = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
     if(number>=0 && number<10){
-        result = arr0_9[number];
-        return result;
+        return first(number);
     }
-    let arr10_19 = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
     if(number>9 && number<20){
         let ostatok=number%10;
         result = arr10_19[ostatok];
         return result;
     }
-    let arr3 = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
     if(number>19 && number<100){
         let ostatok = number%10;
         let decade = Math.floor(number/10);
@@ -20,7 +19,7 @@ module.exports = function toReadable (number) {
             return result;
         }
         if (ostatok>0){
-            result = arr3[decade] + ' ' + arr0_9[ostatok];
+            result = arr3[decade] + ' ' + first(ostatok);
             return result; 
         }
     }
@@ -35,24 +34,24 @@ module.exports = function toReadable (number) {
              return result;
         }
         if(ostatokHundred>0 && ostatokHundred<10) {
-             result = arr0_9[hundred] + ' hundred ' + arr0_9[ostatokHundred];
+             result = first(hundred) + ' hundred ' + first(ostatokHundred);
              return result;  
         }
         if(ostatokHundred>9 && ostatokHundred<20){
          let ostatok=ostatokHundred%10;
-         result = arr0_9[hundred] + ' hundred ' + arr10_19[ostatok];
+         result = first(hundred) + ' hundred ' + arr10_19[ostatok];
          return result;
         }
         if(ostatokHundred>19 && ostatokHundred<100){
          if (ostatok==0){
-             result = arr0_9[hundred] + ' hundred ' + arr3[decade];
+             result = first(hundred) + ' hundred ' + arr3[decade];
              return result;
          }
          if (ostatok>0){
-             result = arr0_9[hundred] + ' hundred ' + arr3[decade] + ' ' + arr0_9[ostatok];
+             result = first(hundred) + ' hundred ' + arr3[decade] + ' ' + first(ostatok);
              return result;  
          }
-         }     
+        }     
      }
-    
+     function first(n){return arr0_9[n];}
 }
